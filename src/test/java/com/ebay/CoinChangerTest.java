6a03036368aa6126fb3240d56eda9108dea0d815
Kajetan Bojko <kajboj@gmail.com>
2016-07-24 12:36:26 +0100

    removed java 8 solution

--- src/test/java/com/ebay/CoinChangerTest.java
+++ src/test/java/com/ebay/CoinChangerTest.java
@@ -1,21 +1,22 @@
 package com.ebay;
 
-import com.google.common.collect.ImmutableMap;
-import org.junit.Test;
+import static java.util.Arrays.asList;
+import static org.junit.Assert.assertEquals;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
-import static com.google.common.collect.Lists.newLinkedList;
-import static java.util.Arrays.asList;
-import static java.util.Map.Entry;
-import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
 
 public class CoinChangerTest {
 
-    Map<Integer, List<Integer>> values = new ImmutableMap.Builder<Integer, List<Integer>>()
-            .put(-1, newLinkedList())
-            .put(0, newLinkedList())
+    Map<Integer, List<Integer>> values = (Map<Integer, List<Integer>>) new ImmutableMap.Builder<Integer, List<Integer>>()
+            .put(-1, ImmutableList.<Integer>of())
+            .put(0, ImmutableList.<Integer>of())
             .put(1, asList(1))
             .put(2, asList(1, 1))
             .put(3, asList(1, 1, 1))
@@ -24,12 +25,7 @@ public class CoinChangerTest {
             .put(34, asList(25, 5, 1, 1, 1, 1))
             .put(84, asList(50, 25, 5, 1, 1, 1, 1))
             .build();
-
-    @Test
-    public void returnsChangeForAmountJava8() {
-        checkChange(new Java8CoinChanger());
-    }
-
+    
     @Test
     public void returnsChangeForAmountJava7() {
         checkChange(new Java7CoinChanger());
